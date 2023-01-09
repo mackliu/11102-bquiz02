7895193b287be29f2a1d406892640a91b4797724
mackliu <mackliu@wda.gov.tw>
2023-01-09 09:11:44 +0800

    完成頁尾版權及跑馬燈功能

--- index.php
+++ index.php
@@ -29,19 +29,31 @@
                	                 </div>
             <div class="hal" id="main">
             	<div>
-            		
+					<marquee style="width:78%; display:inline-block;">
+						請民眾踴躍投稿電子報，讓電子報成為大家相互交流、分享的園地！詳見最新文章	
+				</marquee>
                 	<span style="width:18%; display:inline-block;">
-                    	                    	<a href="?do=login">會員登入</a>
-                    	                    </span>
-                    	<div class="">
-                		                        </div>
+						<a href="?do=login">會員登入</a>
+					</span>
+                    <div class="">
+						<?php
+							$do=$_GET['do']??'home';
+							$file="./front/".$do.".php";
+							if(file_exists($file)){
+								include $file;
+							}else{
+								include "./front/home.php";
+							}
+
+						?>
+					</div>
                 </div>
             </div>
         </div>
         <div id="bottom">
-    	    本網站建議使用：IE9.0以上版本，1024 x 768 pixels 以上觀賞瀏覽 ， Copyright © 2012健康促進網社群平台 All Right Reserved 
+    	    本網站建議使用：IE9.0以上版本，1024 x 768 pixels 以上觀賞瀏覽 ， Copyright © 2023健康促進網社群平台 All Right Reserved 
     		 <br>
-    		 服務信箱：health@test.labor.gov.tw<img src="./home_files/02B02.jpg" width="45">
+    		 服務信箱：health@test.labor.gov.tw<img src="./icon/02B02.jpg" width="45">
         </div>
     </div>
 

7bb6312ed16895320b27c78127f33d94cd71fc25
mackliu <mackliu@wda.gov.tw>
2023-01-09 11:34:32 +0800

    在登入頁撰寫登入的js程式碼

--- front/login.php
+++ front/login.php
@@ -11,8 +11,8 @@
         </tr>
         <tr>
             <td>
-                <button>登入</button>
-                <button>清除</button>
+                <button onclick="login()">登入</button>
+                <button onclick="reset()">清除</button>
             </td>
             <td>
                 <a href="?do=forgot">忘記密碼</a>
@@ -21,3 +21,41 @@
         </tr>
     </table>
 </fieldset>
+
+
+<script>
+
+function reset(){
+    $("#acc,#pw").val('')
+}
+
+function login(){
+    let user={
+        acc:$("#acc").val(),
+        pw:$("#pw").val()
+    }
+
+    $.post("./api/chk_acc.php",user,(result)=>{
+        if(parseInt(result)===1){
+            //有此帳號
+            $.post("./api/chk_pw.php",user,(result)=>{
+                if(parseInt(result)===1){
+                    //帳密正確
+                    if(user.acc==='admin'){
+                        location.href='back.php';
+                    }else{
+                        location.href='index.php';
+                    }
+                }else{
+                    //密碼錯誤
+                    alert("密碼錯誤")
+                }
+            })
+        }else{
+            //無此帳號
+            alert("查無帳號");
+        }
+    })
+}
+
+</script>

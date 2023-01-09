4e4e717ddad9b4f8c868ada714e1c5a04f4f995d
mackliu <mackliu@wda.gov.tw>
2023-01-09 14:45:58 +0800

    完成忘記密碼功能

---
+++ front/forgot.php
@@ -0,0 +1,15 @@
+<div>
+<div>請輸入信箱以查詢密碼</div>
+<div><input type="text" name="email" id="email"></div>
+<div id="result"></div>
+<div><button onclick='forgot()'>尋找</button></div>
+
+</div>
+
+<script>
+function forgot(){
+    $.get('./api/forgot.php',{email:$("#email").val()},(result)=>{
+        $("#result").html(result)
+    })
+}
+</script>

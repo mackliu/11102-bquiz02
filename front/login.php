85b50587da74e8c56e0f874ecbd446030199cc9d
mackliu <mackliu@wda.gov.tw>
2023-01-09 11:06:13 +0800

    建立會員登入頁面

--- front/login.php
+++ front/login.php
@@ -1,1 +1,23 @@
-登入
+<fieldset>
+    <legend>會員登入</legend>
+    <table>
+        <tr>
+            <td>帳號</td>
+            <td><input type="text" name="acc" id="acc"></td>
+        </tr>
+        <tr>
+            <td>密碼</td>
+            <td><input type="password" name="pw" id="pw"></td>
+        </tr>
+        <tr>
+            <td>
+                <button>登入</button>
+                <button>清除</button>
+            </td>
+            <td>
+                <a href="?do=forgot">忘記密碼</a>
+                <a href="?do=reg">尚未註冊</a>
+            </td>
+        </tr>
+    </table>
+</fieldset>

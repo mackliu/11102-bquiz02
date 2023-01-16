a62225e5dd5954acd7a43c9f66c679f838a726b2
mackliu <mackliu@wda.gov.tw>
2023-01-16 14:43:09 +0800

    建立投票畫面

---
+++ front/vote.php
@@ -0,0 +1,30 @@
+<?php
+
+$subject=$Que->find($_GET['id']);
+
+$options=$Que->all(['parent'=>$_GET['id']]);
+
+
+?>
+
+
+<fieldset>
+<legend>目前位置：首頁 > 問卷調查 > <?=$subject['text'];?></legend>
+<form action="./api/vote.php">
+    <h3><?=$subject['text'];?></h3>
+    <?php
+
+    foreach($options as $opt){
+        echo "<p>";
+        echo "<input type='radio' value='{$opt['id']}'>";
+        echo $opt['text'];
+        echo "</p>";
+    }
+
+    ?>
+
+    <div class="ct">
+        <input type="submit" value="我要投票">
+    </div>
+</form>
+</fieldset>
